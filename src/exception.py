1d56bd07f9ffe6de881844203a28d7aff15a5444
hemantsai17 <hemantsai3@gmail.com>
2024-11-25 16:25:43 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+# Sys contains information about python environment altogether
+
+def error_message(error ,error_details:sys):
+   ## exc-tb will give error occured in which part of the python environment
+   _,_,exc_tb =  error_details.exc_info()
+   file_name = exc_tb.tb_frame.f_code.co_filename
+
+   error_msg = 'Error occured in python script name [{0}] line number [{1}] error message [{2}]',format(
+      file_name , exc_tb.tb_lineno , str(error)
+    
+   )
+   return error_msg
+
+
+#This class is inheriting from Exception class present in python
+class CustomException(Exception):
+    def __init__(self, error_message , error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message(error_message , error_details = error_detail)
+
+    def __str__(self) -> str:
+       return self.error_message
+
+
+
+    
+
+# if __name__ == '__main__':
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info('Divide by Zero')
+#         raise CustomException(e , sys)
